c8007d119d5219ae80a760941592c9c36f42fd1a
Andy Pike <andy@andypike.com>
2012-05-10 20:29:25 +0100

    Refactored YouTube data loading into a service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,10 @@ require 'youtube_it'
 
 class HomeController < ApplicationController
   def index
-    #cache = ActiveSupport::Cache::MemoryStore.new
-
-    # client = YouTubeIt::Client.new
-
-    # videos = cache_store.read("videos")
-    # unless videos
-    #   videos = client.videos_by(:user => 'lifesaglitchtv').videos
-    #   cache_store.write("videos", videos)
-    # end
-
-    # @latest_video = videos.first
-    # @videos = videos.take(5).slice(1..4)
+    puts cache_store
+    
+    you_tube = YouTubeService.new(cache_store)
+    @latest_video = you_tube.latest_video
+    @videos = you_tube.recent_videos
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -10,4 +10,21 @@ describe HomeController do
     it { should render_template(:index) }
     it { should_not set_the_flash } 
   end
+
+  context "When loading the YouTube data" do
+    before do
+      @service = double.as_null_object
+      YouTubeService.stub(:new){ @service }
+    end
+
+    it "gets the latest video data" do 
+      @service.should_receive(:latest_video)
+      get :index
+    end
+
+    it "gets the recent videos list" do 
+      @service.should_receive(:recent_videos)
+      get :index
+    end
+  end
 end
