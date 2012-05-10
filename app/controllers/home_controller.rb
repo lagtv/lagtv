1fc10392eea218168e759039a3262fa672c3c462
Andy Pike <andy@andypike.com>
2012-05-10 20:31:03 +0100

    Removed debugging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ require 'youtube_it'
 
 class HomeController < ApplicationController
   def index
-    puts cache_store
-    
     you_tube = YouTubeService.new(cache_store)
     @latest_video = you_tube.latest_video
     @videos = you_tube.recent_videos
