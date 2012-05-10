fd944e425509a76cb45828382a7f8517991981db
George Good <georgegood69@gmail.com>
2012-05-10 18:28:46 +0100

    Started to added tests for homepage controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,15 @@ class HomeController < ApplicationController
   def index
     #cache = ActiveSupport::Cache::MemoryStore.new
 
-    client = YouTubeIt::Client.new
+    # client = YouTubeIt::Client.new
 
-    videos = cache_store.read("videos")
-    unless videos
-      videos = client.videos_by(:user => 'lifesaglitchtv').videos
-      cache_store.write("videos", videos)
-    end
+    # videos = cache_store.read("videos")
+    # unless videos
+    #   videos = client.videos_by(:user => 'lifesaglitchtv').videos
+    #   cache_store.write("videos", videos)
+    # end
 
-    @latest_video = videos.first
-    @videos = videos.take(5).slice(1..4)
+    # @latest_video = videos.first
+    # @videos = videos.take(5).slice(1..4)
   end
 end
--- spec/acceptance/homepage_renders_spec.rb
+++ spec/acceptance/homepage_renders_spec.rb
@@ -2,9 +2,9 @@ require 'acceptance/acceptance_helper'
 
 feature 'Homepage renders' do
 
-  scenario 'welcome message appears' do
+  scenario 'upload button message appears' do
     visit '/'
-    page.should have_content('Welcome to LagTV!')
+    page.should have_content('Upload')
   end
 
 end
