3a071e8dcef953a234c962de49dab732f8467b0f
Andy Pike <andy@andypike.com>
2012-05-12 00:32:41 +0100

    Fixed unicorn listening socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.lagtv.sock"
 worker_processes 2
 timeout 30
