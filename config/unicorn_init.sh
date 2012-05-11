c025b49b3f54efb4b8b8d478804bc4f3bacf17f6
Andy Pike <andy@andypike.com>
2012-05-11 23:23:12 +0100

    Fixed unicorn config

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,10 +12,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/blog/current
+APP_ROOT=/home/apps/lagtv/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deployer
+AS_USER=root
 set -u
 
 OLD_PIN="$PID.oldbin"
