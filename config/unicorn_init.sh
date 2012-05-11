53a4b791ecb6deee796369811b06852b917cf010
Andy Pike <andy@andypike.com>
2012-05-11 23:59:45 +0100

    Added unicorn debugging flag

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -14,7 +14,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/apps/lagtv/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production -d"
 AS_USER=root
 set -u
 
