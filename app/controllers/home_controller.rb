31a8d6d7fb00d8dcd6d7d64bf352b029766c0339
Andy Pike <andy@andypike.com>
2012-05-13 22:17:04 +0100

    Added minecraft channel content to homepage tab

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     lifesaglitchtv = YouTubeService.new('lifesaglitchtv', cache_store)
+    lifesaglitchtv2 = YouTubeService.new('lifesaglitchtv2', cache_store)
     @lifesaglitchtv = lifesaglitchtv.channel
+    @lifesaglitchtv2 = lifesaglitchtv2.channel
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -13,12 +13,19 @@ describe HomeController do
 
   context "When loading the YouTube data" do
     before do
-      @lagtv_service = double.as_null_object
-      YouTubeService.stub(:new).with('lifesaglitchtv', @controller.cache_store) { @lagtv_service }
+      @lagtv1_service = double.as_null_object
+      @lagtv2_service = double.as_null_object
+      YouTubeService.stub(:new).with('lifesaglitchtv', @controller.cache_store) { @lagtv1_service }
+      YouTubeService.stub(:new).with('lifesaglitchtv2', @controller.cache_store) { @lagtv2_service }
     end
 
     it "gets the YouTube channel data for lifesaglitchtv" do 
-      @lagtv_service.should_receive(:channel)
+      @lagtv1_service.should_receive(:channel)
+      get :index
+    end
+
+    it "gets the YouTube channel data for lifesaglitchtv2" do 
+      @lagtv2_service.should_receive(:channel)
       get :index
     end
   end
