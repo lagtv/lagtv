8a07c03c5141667b9e1adae0a0b7d29b5ecb5299
Andy Pike <andy@andypike.com>
2012-05-20 22:42:15 +0100

    Added user active attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120520125056) do
+ActiveRecord::Schema.define(:version => 20120520213352) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120520125056) do
     t.datetime "created_at",                            :null => false
     t.datetime "updated_at",                            :null => false
     t.string   "role",            :default => "Member", :null => false
+    t.boolean  "active",          :default => true,     :null => false
   end
 
 end
