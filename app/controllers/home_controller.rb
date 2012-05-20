6be52f7dc0e10f9cb5287c11cef6d24b6547f8f9
FxN <fxnstyling@gmail.com>
2012-05-20 17:34:47 +0100

    Added registration form to homepage, changed what is displayed on home page depending if you're logged in or not

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,9 @@ class HomeController < ApplicationController
     lifesaglitchtv2 = YouTubeService.new('lifesaglitchtv2', cache_store)
     @lifesaglitchtv = lifesaglitchtv.channel
     @lifesaglitchtv2 = lifesaglitchtv2.channel
+
+    if env['omniauth.identity']
+    	@user = env['omniauth.identity']
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < OmniAuth::Identity::Models::ActiveRecord
   has_secure_password
+  validates_presence_of :email, :name
+  validates_uniqueness_of :email
+
   attr_accessible :email, :name, :password, :password_confirmation
 end
