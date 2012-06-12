8e4b91e71db0393461afd8b5d956e1480817c1d1
Andy Pike <andy@andypike.com>
2012-06-12 23:12:27 +0100

    Added newrelic initializer to get it working with unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,6 @@ working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
-preload_app true
 
 listen "/tmp/unicorn.lagtv.sock"
 worker_processes 2
