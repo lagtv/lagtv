13bf921b3bff482b2962518c5aefe83bd3a1f702
Andy Pike <andy@andypike.com>
2012-07-01 16:46:32 +0100

    Removed commented out code

--- lib/forum_formatter_markdown.rb
+++ lib/forum_formatter_markdown.rb
@@ -1,7 +1,5 @@
 class ForumFormatterMarkdown
   def self.format(content)
-    #Markdown.new(content).to_html.html_safe
-
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true)
     markdown.render(content).html_safe
   end
