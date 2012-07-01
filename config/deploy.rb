1b3db39661871ef0b947da1fb161b184c50086b7
Andy Pike <andy@andypike.com>
2012-07-01 01:08:45 +0100

    Added db migrate to the deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,7 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
+after "deploy", "deploy:migrate"
 
 namespace :deploy do
   %w[start stop restart].each do |command|
