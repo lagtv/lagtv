eb995cfc83c298eebaaea1a301ce66123969e1e4
Andy Pike <andy@andypike.com>
2012-07-14 11:20:26 +0100

    Added host config to production to fix issue 47

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Lagtv::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'lag.tv' }
 
   # Enable threaded mode
   # config.threadsafe!
