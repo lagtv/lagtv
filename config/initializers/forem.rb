548b89d4a15bdca00f6aa0b8ca4df7de46ee9d01
Andy Pike <andy@andypike.com>
2012-07-14 11:38:47 +0100

    Changed the from email address to a no-reply lag tv one

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,5 +1,5 @@
 Forem.user_class = "User"
-Forem.email_from_address = "please-change-me@example.com"
+Forem.email_from_address = "\"LagTV Forum\" <no-reply@lag.tv>"
 Forem.per_page = 20
 Forem.sign_in_path = "/login"
 Forem.formatter = ForumFormatterSafeHtml
