3003a286a02b4204641a49b3340d2880f70be93d
Andy Pike <andy@andypike.com>
2012-10-29 00:34:56 +0000

    Added GA and also updated the about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,22 @@ module ApplicationHelper
       end      
     end
   end  
+
+  def google_analytics
+    if CONFIG[:ga_tracker].present? 
+      "<script type='text/javascript'>
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '#{CONFIG[:ga_tracker]}']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>".html_safe
+    end
+  end
 end
