2db76de12da853bd327281baee261566e3aaec02
Andy Pike <andy@andypike.com>
2012-10-31 22:49:33 +0000

    Fixed the ordering of latest topics on the homepage

--- lib/forum_service.rb
+++ lib/forum_service.rb
@@ -1,6 +1,20 @@
 class ForumService
   def self.latest_topics
-    Forem::Topic.where(:state => 'approved', :hidden => false).order('updated_at desc').limit(10)
+    # The Forem::Topic#last_post_at attribute isn't being updated when new posts are added to a topic, 
+    # so need to craft a query based on updated_at of posts
+    Forem::Topic.joins("inner join forem_posts fp on (forem_topics.id = fp.topic_id)")
+        .where(:state => 'approved', :hidden => false)
+        .where("fp.state = 'approved'")
+        .group('forem_topics.id')
+        .order("max(fp.updated_at) desc")
+        .select("forem_topics.*")
+
+    # Should build this query:
+    #   select ft.* from forem_topics ft inner join forem_posts fp on (ft.id = fp.topic_id) 
+    #   where ft.state = 'approved' and ft.hidden = 'f' 
+    #   and fp.state = 'approved' 
+    #   group by ft.id 
+    #   order by max(fp.updated_at) desc;
   end
 
   def self.latest_posts(page = 1)
---
+++ spec/lib/forum_service_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe ForumServiceSpec do
+  context "When retrieving the list of recently updated topics" do
+    
+  end
+end
