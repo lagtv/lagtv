12e6134ceb32ac441bec9662d2f63b8090091587
Kevin Hall <khall@liveops.com>
2012-12-19 16:48:34 -0800

    More tests, added a show email page for admins to keep tab on the sending status, added backgrounding gem, updated emails table to have total recipients

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,24 @@ class UserMailer < ActionMailer::Base
     @user = user
     mail :to => user.email, :subject => "Password Reset for LAGTV Website"
   end
+
+  def group_message(email)
+    sample_mail = nil
+    count = 0
+    total_users = User.send(email.roles).count
+
+    while(count < total_users) do
+      email.update_attribute(:total_sent, count)
+      users = User.send(email.roles).order(:id).limit(MASS_EMAIL_STEPPING).offset(count)
+      users.each do |u|
+        sample_mail = mail :to => u.email, :subject => email.subject do |format|
+          format.text { render :text => email.body }
+        end
+        count += 1
+      end
+    end
+    sample_mail
+  ensure
+    email.update_attribute(:total_sent, count)
+  end
 end
--- spec/fabricators/email_fabricator.rb
+++ spec/fabricators/email_fabricator.rb
@@ -2,6 +2,9 @@ Fabricator(:email) do
   id { sequence(:id) }
   subject { "Weekly Update #1" }
   body { "Hey there, hi there, ho there." }
-  total_sent { rand(10000) }
-  roles { User::ROLES.sample( [1, rand(User::ROLES.length)].max ) }
+  started_at { Time.now - 200 - rand(1000) }
+  ended_at { rand(2) == 1 ? Time.now - rand(150) : nil }
+  total_sent { rand(1000) }
+  total_recipients { 1000 + rand(10000) }
+  roles { User::ROLES.sample( [1, rand(User::ROLES.length)].max ).join(' ') }
 end
