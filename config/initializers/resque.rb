802a458e8a42cb8b7874e1a404ec12d36ca737ac
Andy Pike <andy@andypike.com>
2013-01-24 20:56:53 +0000

    Added db connection fix for resque workers

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,3 +3,5 @@ require 'resque/server'
 Resque::Server.use(Rack::Auth::Basic) do |user, password|  
   password == CONFIG[:resque_password]
 end
+
+Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
