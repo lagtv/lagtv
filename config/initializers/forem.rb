9df8f28c958df7bcf5de1396a75a72bd0dfb142e
Andy Pike <andy@andypike.com>
2013-08-04 14:17:48 +0100

    Removed Forem profile links from config and adjusted indents in the PR from #292

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -3,7 +3,7 @@ Forem.email_from_address = "\"LAGTV Forum\" <no-reply@lag.tv>"
 Forem.per_page = 20
 Forem.sign_in_path = "/login"
 Forem.formatter = ForumFormatterSafeHtml
-Forem.user_profile_links = true
+Forem.user_profile_links = false
 
 Rails.application.config.to_prepare do
   Forem::Ability.register_ability(Ability)
