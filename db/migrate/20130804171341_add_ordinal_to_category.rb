ef8813942670791521ab3d4f2698ed4c7335e188
Andy Pike <andy@andypike.com>
2013-08-04 22:16:59 +0100

    Fixed category migration so it runs on UAT which has different categories

--- db/migrate/20130804171341_add_ordinal_to_category.rb
+++ db/migrate/20130804171341_add_ordinal_to_category.rb
@@ -2,13 +2,13 @@ class AddOrdinalToCategory < ActiveRecord::Migration
   def change
     add_column :forem_categories, :ordinal, :integer
 
-    Forem::Category.find_by_name("LAG TV").update_attributes(:ordinal => 1)
-    Forem::Category.find_by_name("Off-Topic").update_attributes(:ordinal => 2)
-    Forem::Category.find_by_name("E-Sports").update_attributes(:ordinal => 3)
-    Forem::Category.find_by_name("Gaming").update_attributes(:ordinal => 4)
-    Forem::Category.find_by_name("Platforms").update_attributes(:ordinal => 5)
-    Forem::Category.find_by_name("Tech").update_attributes(:ordinal => 6)
-    Forem::Category.find_by_name("Art").update_attributes(:ordinal => 7)
-    Forem::Category.find_by_name("Support").update_attributes(:ordinal => 8)
+    Forem::Category.find_by_name("LAG TV").try(:update_attributes, {:ordinal => 1})
+    Forem::Category.find_by_name("Off-Topic").try(:update_attributes, {:ordinal => 2})
+    Forem::Category.find_by_name("E-Sports").try(:update_attributes, {:ordinal => 3})
+    Forem::Category.find_by_name("Gaming").try(:update_attributes, {:ordinal => 4})
+    Forem::Category.find_by_name("Platforms").try(:update_attributes, {:ordinal => 5})
+    Forem::Category.find_by_name("Tech").try(:update_attributes, {:ordinal => 6})
+    Forem::Category.find_by_name("Art").try(:update_attributes, {:ordinal => 7})
+    Forem::Category.find_by_name("Support").try(:update_attributes, {:ordinal => 8})
   end
 end
